8556982a4196caba7bcfd4b7ba25d66968f1d2d4
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-04-16 19:39:17 -0500

    creating users schema

---
+++ models/Users.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: 10
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true, 
+            validate: email,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thoughts"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            }
+        ],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const Users = model("user", userSchema);
+
+module.exports = Users; 
