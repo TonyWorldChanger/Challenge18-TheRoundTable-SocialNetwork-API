6c0ba72031e4ac7db199f52621305996527aa686
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-04-30 12:58:28 -0500

    creating data.js file

---
+++ utils/data.js
@@ -0,0 +1,28 @@
+const users = [
+    "Joe",
+    "Matt",
+    "Tony",
+    "Paris",
+    "Kim",
+];
+
+const email = [
+  "joe@email.com",
+  "matt@email.com",
+  "tony@email.com",
+  "paris@email.com",
+  "kim@email.com",
+
+];
+
+const thoughts = [
+    "Joe is crushing it!",
+    "This is really hard work.",
+    "You have to practice to get good at something.",
+    "I wonder if we will be able to go to Mars one day?",
+    "I will change the world and have a good impact.",
+    "I want to be rich enough to do what I want lol.",
+];
+
+
+module.exports = { users, email, thoughts };
