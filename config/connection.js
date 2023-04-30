1a89018ea86351cb1869901d8dd7d1566215a291
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-04-30 17:34:14 -0500

    made db name change

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require("mongoose");
 
 const connectionString = 
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Challenge18-TheRoundTable-SocialNetwork-API";
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/SocialNetworkdb";
 
     connect(connectionString, {
         useNewUrlParser: true,
