eedd7d7ac7437fb0b458dd10572751f619e9ec0e
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-04-30 16:40:05 -0500

    had to rename the index.js file in routes folder

--- models/Users.js
+++ models/Users.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true, 
-            validate: email,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
         },
         thoughts: [
             {
